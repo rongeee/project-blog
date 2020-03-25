17ce745760f8f260653a322f18605bbe439ea696
Patric Ronge <pronge@gmail.com>
2020-03-25 14:43:17 +0100

    generate blog posts?

--- pages/blog.php
+++ pages/blog.php
@@ -1,3 +1,35 @@
 <div class="container">
     <h1>pls crate nice blog content here thank u</h1>
+
+    <?php
+    require_once 'db.php';
+    $stmt = $db->prepare("SELECT * FROM proj_posts");
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $id= $row['id'];
+        $title = $row['title'];
+        $image = $row['image'];
+        $author = $row['author'];
+        $date = $row['date'];
+        $msg = $row['msg'];
+
+        if (empty($image)) {
+            // CHANGE IMG URL RETARD
+            $image= "images/no-poster.png";
+            } else {
+                $image = "images/$image";
+            };
+    ?>
+
+    <div class="blog-entry">
+        <div class="blog-entry__header">
+            <h2 class="blog-entry__title"><?= $title; ?></h2>
+            <small class="blog-entry__date"><?= $date; ?></small>
+        </div>
+        <img class="blog-entry__image" src="<?= $image; ?>" alt="<?= $title; ?>">
+        <p class="blog-entry__msg"><?= $msg; ?></p>
+        <small class="blog-entry__author"><?= $author; ?></small>
+    </div>
+
 </div>
