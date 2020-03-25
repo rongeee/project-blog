ec65e3dbe4c0e0fc40a2be4e7d81ca226a1bd1c8
Samuel Mårtensson <samuel137@live.se>
2020-03-25 14:50:35 +0100

    file upload

--- db/add.php
+++ db/add.php
@@ -1,19 +1,25 @@
 <?php
 
     require "db.php";
-
     echo $_SERVER['REQUEST_METHOD'];
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $title = htmlspecialchars( $_POST['title']);
         $author = htmlspecialchars( $_POST['author']);
         $message = htmlspecialchars( $_POST['message']);
         
-        $sql = "INSERT INTO proj_posts(title, author, message) VALUES (:title, :author, :message)";
+        require "upload.php";
+        $file_upload = htmlspecialchars( $_POST['fileToUpload']);
+        $embed = htmlspecialchars( $_POST['embed']);
+
+        
+        $sql = "INSERT INTO proj_posts(title, author, message, image) VALUES (:title, :author, :message, :image, :embed)";
 
         $stmt = $db->prepare($sql);
         $stmt->bindParam(":title", $title);
         $stmt->bindParam(":author", $author);
         $stmt->bindParam(":message", $message);
+        $stmt->bindParam(":image", $file_upload);
+        $stmt->bindParam(":embed", $embed);
         $stmt->execute();
     }
 
@@ -21,11 +27,13 @@
 ?>
 
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" enctype="multipart/form-data">
     <h4>Create post</h4>
     <input type="text" name="title">
     <input type="text" name="author">
     <input type="text" name="message">
+    <input type="file" name="fileToUpload">
+    <input type="text" name="embed">
     <input type="submit"
     class="form-control my-2 btn btn-outline-success"
     value="Publish">
