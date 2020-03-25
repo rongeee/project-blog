f576a64e717eb897ba74e2b6677619084eec4b17
Samuel Mårtensson <samuel137@live.se>
2020-03-25 18:14:27 +0100

    fixed file structure and relocateed admin files

---
+++ pages/admin_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nice Blog</title>
+    <link rel="stylesheet" href="../../sass/main.css">
+</head>
+<body>
+    <header class="header">
+        <h1 class="header__logo"><a href="../../index.php">Blog</a></h1>
+        <a href="./admin.php" class="header__admin-link"><p class="header__admin-text">Admin</p></a>
+    </header>
