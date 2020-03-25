8106655ddb76cf3b99b9ade90b4ed7ff36db5674
Samuel Mårtensson <samuel137@live.se>
2020-03-25 14:27:24 +0100

    added database and post form

--- db/db.php
+++ db/db.php
@@ -1,2 +1,19 @@
 <?php
+
+
+    $db_server = "localhost";
+    $db_database = "project";
+    $db_username = "root";
+    $db_password = "root";
+    try{
+    $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8"
+    ,
+    $db_username, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+    echo $e-> getMessage();
+    }
+
+
 ?>
